6b153e99049041857e2bb491fc9893c4e08d6586
nilindra001@gmail.com <{your_email}>
2026-04-11 20:17:13 +0530

    Add Nav component for navigation layout

---
+++ src/lib/components/Nav.svelte
@@ -0,0 +1,20 @@
+<div class="flex h-full w-full flex-col items-center gap-4">
+	<div class="flex w-full items-center justify-center pt-4">
+		<div
+			class="flex h-10 w-10 items-center justify-center
+              rounded-lg bg-accent"
+		>
+			<span class="font-display text-lg font-medium text-white">Flō</span>
+		</div>
+	</div>
+	<div class="h-full">
+		<ul
+			class="flex h-full flex-col items-center justify-center
+            gap-4 px-2 py-2 text-sm"
+		>
+			<!-- <li>Home</li> -->
+			<!-- <li>About</li>
+			<li>Contact</li> -->
+		</ul>
+	</div>
+</div>
